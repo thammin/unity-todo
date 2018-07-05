9a8bbbca34c52a5f8b726a5f0d9b24db355d5b5b
thammin <rim_paulyoung@cyberagent.co.jp>
2018-07-05 19:09:22 +0900

    Add DataBind sample

---
+++ Assets/TodoDataBind/TodoDataBind.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TodoDataBind
+{
+    public class TodoDataBind : MonoBehaviour
+    {
+
+        private DataBindContext _context;
+        private ObservableList _items;
+
+        [SerializeField]
+        private InputField _input;
+
+        private void Awake()
+        {
+            _context = GetComponent<DataBindContext>();
+            _items = new ObservableList("Items");
+            _context["Items"] = _items;
+        }
+
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        private void Start()
+        {
+            // 本来はWorkからデータをモデルを作るけど割愛する
+            for (var i = 0; i < 5; i++)
+            {
+                _items.Add(new TodoDataBindItemModel
+                {
+                    Description = $"item {i}"
+                });
+            }
+        }
+
+        public void AddItem()
+        {
+            _items.Add(new TodoDataBindItemModel
+            {
+                Description = _input.text
+            });
+            _input.text = "";
+        }
+    }
+}
+
---
+++ Assets/TodoDataBind/TodoDataBindItem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TodoDataBind
+{
+    public class TodoDataBindItemModel
+    {
+        // Propertyが必要である
+        public string Description { get; set; }
+    }
+
+    public class TodoDataBindItem : MonoBehaviour
+    {
+        [SerializeField]
+        private Image _checkMark;
+
+        public void SetCheckMark(bool isOn)
+        {
+            _checkMark.enabled = isOn;
+        }
+    }
+}
+
