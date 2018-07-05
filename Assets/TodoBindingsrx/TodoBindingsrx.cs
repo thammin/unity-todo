76e2886667f720ea333ded80846af837b0a40708
thammin <rim_paulyoung@cyberagent.co.jp>
2018-07-05 17:39:47 +0900

    Add bindingsrx sample

---
+++ Assets/TodoBindingsrx/TodoBindingsrx.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using BindingsRx.Bindings;
+
+namespace TodoBindingsrx
+{
+    public class TodoBindingsrx : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _itemPrefab;
+
+        [SerializeField]
+        private Text _temp;
+
+        [SerializeField]
+        private InputField _input;
+
+        [SerializeField]
+        private Button _button;
+
+        [SerializeField]
+        private Transform _listContainer;
+
+        private List<TodoBindingsrxItemModel> _models;
+
+        /// <summary>
+        /// 初期化(本来はIEnumeratorのInitializeだけど割愛する)
+        /// </summary>
+        public void Start()
+        {
+            // 本来はWorkからデータをモデルを作るけど割愛する
+            _models = new List<TodoBindingsrxItemModel>();
+            for (var i = 0; i < 5; i++)
+            {
+                AddItem($"item {i}");
+            }
+
+            BindData();
+            BindEvents();
+        }
+
+        private void BindData()
+        {
+            _temp.BindTextTo(() => _input.text);
+        }
+
+        private void BindEvents()
+        {
+            _button.OnClickAsObservable()
+                .Subscribe(u =>
+                {
+                    AddItem(_input.text);
+                    _input.text = "";
+                })
+                .AddTo(this);
+        }
+
+        private void AddItem(string description)
+        {
+            var model = new TodoBindingsrxItemModel()
+            {
+                Description = description
+            };
+            _models.Add(model);
+
+            var instance = Instantiate(_itemPrefab);
+            var presenter = instance.GetComponent<TodoBingindsrxItem>();
+            presenter.Initialize(model);
+            instance.transform.SetParent(_listContainer, false);
+        }
+
+    }
+}
+
