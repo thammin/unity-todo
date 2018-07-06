4f79d7bc6273da1e663110041e20fea9cb995b76
thammin <rim_paulyoung@cyberagent.co.jp>
2018-07-06 14:11:55 +0900

    Add MarkLight sample

---
+++ Assets/TodoMarkLight/TodoMarkLight.cs
@@ -0,0 +1,38 @@
+﻿using MarkLight;
+
+namespace TodoMarkLight
+{
+    public class TodoMarkLight : View
+    {
+        public _string Input;
+
+        public ObservableList<TodoMarkLightItemModel> Items;
+
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            // 本来はWorkからデータをモデルを作るけど割愛する
+            Items = new ObservableList<TodoMarkLightItemModel>();
+            for (var i = 0; i < 5; i++)
+            {
+                Items.Add(new TodoMarkLightItemModel
+                {
+                    Description = $"item {i}"
+                });
+            }
+        }
+
+        public void AddItem()
+        {
+            Items.Add(new TodoMarkLightItemModel
+            {
+                Description = Input.Value
+            });
+            Input.Value = "";
+        }
+    }
+}
---
+++ Assets/TodoMarkLight/TodoMarkLightItem.cs
@@ -0,0 +1,22 @@
+﻿using MarkLight;
+using MarkLight.Views.UI;
+
+namespace TodoMarkLight
+{
+    public class TodoMarkLightItemModel
+    {
+        public string Description;
+    }
+
+    public class TodoMarkLightItem : UIView
+    {
+        public _bool IsCheck;
+
+        public _bool IsShowCheckMark;
+
+        public void ToggleCheckMark()
+        {
+            IsShowCheckMark.Value = IsCheck.Value;
+        }
+    }
+}
